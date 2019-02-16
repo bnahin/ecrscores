d6e5397ddb739411b55a2aa4df275c63fd28f830
Blake Nahin <bnahin@live.com>
2019-02-16 15:39:35 -0800

    Finish Homepage Charts

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Helpers\Helper;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
@@ -10,6 +11,7 @@ class ChartsController extends Controller
 {
     /**
      * Get Pie charts for levels
+     *
      * @param string $level Level string (ela|math)
      *
      * @return \Illuminate\Http\JsonResponse
@@ -30,19 +32,69 @@ class ChartsController extends Controller
         return response()->json($levels);
     }
 
-    //Bar charts...
+    /**
+     * Get Average charts
+     *
+     * @param string $exam
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getAverages(string $exam): JsonResponse {
+        switch($exam) {
+            case "psat": return $this->getPsatAverages();
+            case "sbac": return $this->getSBACAverages();
+        }
+    }
 
     /**
      * Get PSAT Averages Bar Chart - Stacked
      */
-    public function getPsatAverages() {
+    private function getPsatAverages()
+    {
+        $years = [];
+        $math = [];
+        $reading = [];
 
+        $tYears = Auth::user()->getYears();
+        foreach ($tYears as $year) {
+            $years[] = Helper::getFullYearString($year);
+
+            $students = Auth::user()->psatStudents()
+                ->where('year', $year);
+            if (!$students->exists()) {
+                continue;
+            }
+
+            $math[] = round($students->pluck('math')->avg());
+            $reading[] = round($students->pluck('readwrite')->avg());
+        }
+
+        return response()->json(['math' => $math, 'reading' => $reading, 'years' => $years]);
     }
 
     /**
      * Get SBAC Averages Bar Chart
      */
-    public function getSBACAverages() {
+    private function getSBACAverages()
+    {
+        $years = [];
+        $math = [];
+        $ela = [];
 
+        $tYears = Auth::user()->getYears();
+        foreach ($tYears as $year) {
+            $years[] = Helper::getFullYearString($year);
+
+            $students = Auth::user()->sbacStudents()
+                ->where('year', $year);
+            if (!$students->exists()) {
+                continue;
+            }
+
+            $math[] = round($students->pluck('math_scale')->avg());
+            $ela[] = round($students->pluck('ela_scale')->avg());
+        }
+
+        return response()->json(['math' => $math, 'ela' => $ela, 'years' => $years]);
     }
 }
